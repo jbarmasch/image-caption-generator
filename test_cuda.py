27bc771c5b637bfc79e058ef6531114ef6140385
Juan Barmasch <jbarmasch01@gmail.com>
2024-09-25 21:27:10 +0200

    remove unused code

--- test_cuda.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA is available.")
-    print(f"CUDA version: {torch.version.cuda}")
-else:
-    print("CUDA is not available.")
--- try.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-# Print torch version
-print(torch.__version__)
-print(torch.cuda.is_available())
