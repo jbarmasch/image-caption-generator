c3846c26ae3a77ef6322cbcf9abd921081866ad3
Juan Barmasch <jbarmasch01@gmail.com>
2024-12-03 18:23:21 +0100

    Final graphs code

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-import torch
+# import torch
 from pathlib import Path
 
 # Moondream
 ORIGINAL_MODEL = "vikhyatk/moondream2"
 MOONDREAM_VERSION = "2024-07-23"
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-DTYPE = torch.float16 if torch.cuda.is_available() else torch.float32
+# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+# DTYPE = torch.float16 if torch.cuda.is_available() else torch.float32
 
 # Fine-tuning
-EPOCHS = 3
+EPOCHS = 1
 BATCH_SIZE = 16
 GRAD_ACCUM_STEPS = 1
 LR = 3e-5
@@ -23,5 +23,8 @@ ADAM_EPS = 1e-6
 
 # Metrics
 MAX_METRIC_ITER = 150
-METRICS_DIR = Path('./Training results/Metrics/Moondream')
-TEMPERATURES = [None, 0.3, 0.7]
+METRICS_DIR = Path('/home/juanmb/Desktop/UASTW/Metrics and graphs/Last compare')
+TEMPERATURES = [0.3, 0.7]
+
+# Graphs
+GRAPH_DIR = Path('./Training results/Graphs/Moondream')
